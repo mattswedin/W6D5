5179b60da0072eb97726c4cf28a3a3b1ee3189a9
Matthew Swedin <matthew.swedin@gmail.comgit config --global user.email matthew.swedin@gmail.com>
2021-09-17 14:02:42 -0400

    table & model

---
+++ 99Cats/app/models/cat.rb
@@ -0,0 +1,26 @@
+require 'action_view'
+
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+
+    validates :birth_date, :color, :name, :sex, presence: true
+    validates :color, inclusion: { in: ["orange", "white", "black", "brown", "rainbow"], message: "invalid color entered"}
+    validates :sex, inclusion: { in: ["m", "f"], message: "invalid sex entered" } 
+
+    def age
+        distance_of_time_in_words_to_now(self.birth_date)
+    end
+end
